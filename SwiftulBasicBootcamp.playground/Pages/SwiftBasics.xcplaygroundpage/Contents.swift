6ffd07af9009bb1da6e5008fb0f310be5ce72a80
Agung Nawawi <agung@fairtech.com.sg>
2023-08-02 20:59:44 +0700

    lesson 1 - Swift Basics

---
+++ SwiftulBasicBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+// print to console
+print(greeting)
+
+
+// constant
+let camelCase = "Hello Ajung"
+print(camelCase)
+
+let PascalCase = "John Doe"
+
+let Snake_Case = "Ini Contoh penulissan variabel   "
