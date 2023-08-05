36234cf746232f2b4708ed4e6be433740b07c28b
Agung Nawawi <agung@fairtech.com.sg>
2023-08-05 20:31:07 +0700

    if statement

--- SwiftulBasicBootcamp.playground/Pages/IfStatement.xcplaygroundpage/Contents.swift
+++ SwiftulBasicBootcamp.playground/Pages/IfStatement.xcplaygroundpage/Contents.swift
@@ -15,10 +15,72 @@ if !isUserPremium == true {
 }
 
 
+// if-else statement
 var isUserActive = false
 
 if isUserActive {
     print("User Sedang Aktif")
 } else {
     print("User sedang Tidak Aktif")
+}
+
+
+// if-else-if statement
+var nilai = 80
+
+if (nilai >= 90) {
+    print("Excelent")
+} else if (nilai >= 80) {
+    print("Great")
+} else if (nilai >= 70) {
+    print("Good")
+} else {
+    print("Try Again")
+}
+
+// nested if-else statement
+var a = 100
+var b = 200
+var c = 150
+
+
+print("Nilai A:",a,", Nilai B:",b,", NilaiC:",c)
+// outer if statement
+if (a > b) {
+    // inner if statement
+    if (a > c) {
+        //print statement 1
+        print("Nilai A lebih besar")
+    } else { // inner else
+        // print statement 2
+        print("Nilai C lebih besar")
+    }
+} else { // outer else
+    // inner if statement
+    if (b > c) {
+        print("Nilai B lebih besar")
+    } else {
+        print("Nilai C Lebih besar")
+    }
+}
+
+
+var likeCount = 20
+var commentCount = 00
+var viewCount = 50
+
+
+// AND
+if likeCount > 3 && commentCount > 0 {
+    print("Post memiliki lebih dari 3 like DAN memiliki komentar ")
+} else {
+    print("Post kurang dari 3 yang suka ATAU belum ada komentar")
+}
+
+
+// OR
+if likeCount > 10 || viewCount > 0 {
+    print("Post memiliki lebih dari 10 like ATAU memiliki komentar ")
+} else {
+    print("Post tidak memiliki LIKE lebih dari 3 like ATAU memiliki komentar ")
 }
--- SwiftulBasicBootcamp.playground/Pages/Operator.xcplaygroundpage/Contents.swift
+++ SwiftulBasicBootcamp.playground/Pages/Operator.xcplaygroundpage/Contents.swift
@@ -1,3 +1,59 @@
 import Foundation
 
 
+var likeCount: Double = 20
+var commentCount: Double = 4
+var viewCount: Double = 3
+
+// penjumlahan
+// likeCount = likeCount + 3
+// likeCount += 3
+
+// pengurangan
+//likeCount = likeCount - 5
+//likeCount += 7
+
+// perkalian
+//likeCount = likeCount * 2
+//likeCount *= 3
+
+// pembagian
+//likeCount = likeCount / 4
+//likeCount /= 2
+
+// Order of Operations
+//likeCount = likeCount - 2 * 3;
+//viewCount = (viewCount + 6) * 3
+
+var nilaiA = 40
+var nilaiB = 69
+var nilaiC = 80
+var average = (nilaiA + nilaiB + nilaiC) / 3
+
+if nilaiA == 55 {
+    print("Selamat anda lulus")
+} else {
+    print("Anda Tidak Lulus")
+}
+
+if nilaiB != 55 {
+    print("Selamat anda diterima")
+} else {
+    print("anda gagal")
+}
+
+if average > 60 {
+    print("Good, nilai rata-rata anda",average)
+} else {
+    print("Try Again")
+}
+
+if nilaiA >= 60 {
+    print("Nilai A >= 60")
+}
+
+if (nilaiA > 50 ) && (nilaiB > 70) {
+    print("Nilai A anda:",nilaiA, "dan Nilai B anda:",nilaiB)
+} else {
+    print("Coba lagi")
+}
