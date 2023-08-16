0e4b57a4376f24224b2cf78cc99f626dc7dc9c11
Agung Nawawi <agung@fairtech.com.sg>
2023-08-16 13:52:59 +0700

    Structs - Customizing Init

--- SwiftulBasicBootcamp.playground/Pages/Structs.xcplaygroundpage/Contents.swift
+++ SwiftulBasicBootcamp.playground/Pages/Structs.xcplaygroundpage/Contents.swift
@@ -28,3 +28,48 @@ let myQuiz: Quiz = Quiz(judul: "Quiz 1", tanggalDibuat: .now)
 
 print(myQuiz.title) // mengakses Property "title" pada object "myQuiz"
 print(myQuiz.dateCreated) // mengakses Property "dateCreated" pada object "myQuiz"
+
+
+// ######### ---------- Customizing Initializer ---------- ##########
+// membuat object Quiz2
+struct Quiz2 {
+    // Property pada object
+    let title: String
+    let taskCount: Int
+    let dateCreated: Date
+    let isPremium: Bool?
+    
+    // Structs have an implicit init
+    // Swift membuat init otomatis tanpa harus membuat func seperti dibawah
+    // "judul" & "tanggalDibuat" = parameter pada func init
+    
+    // kostumisasi init
+    // parameter "tanggalDibuat" diberikan default value
+    init(judul: String, jumlahSoal: Int, tanggalDibuat: Date = .now, isPremium: Bool?) {
+        self.title = judul // title = judul "parameter pada initializer"
+        self.taskCount = jumlahSoal // title = judul "parameter pada initializer"
+        self.dateCreated = tanggalDibuat // dateCreated = tanggalDibuat  "parameter pada initializer"
+        self.isPremium = isPremium
+    }
+}
+
+let myQuiz2: Quiz2 = Quiz2(judul: "Matematika", jumlahSoal: 99, isPremium: nil)
+print(myQuiz2)
+
+
+struct Car {
+    let carManufacture: String
+    let year: Date
+    let carType: String
+    let engine: String?
+    
+    init(carManufacture: String, year: Date?, carType: String, engine: String?) {
+        self.carManufacture = carManufacture
+        self.year = year ?? .now // property "year" bertipe data bukan optionals, tetapi di parameter func init bertype optional. jadi harus di unwrap dulu.
+        self.carType = carType
+        self.engine = engine
+    }
+}
+
+let myCar: Car = Car(carManufacture: "Toyota", year: nil, carType: "SUV", engine: "Diesel")
+print(myCar)
