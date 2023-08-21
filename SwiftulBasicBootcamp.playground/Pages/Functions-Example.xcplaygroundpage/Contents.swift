2320ec36d1caae26b739463473c9cfe83d63b80b
Agung Nawawi <agung@fairtech.com.sg>
2023-08-21 20:59:26 +0700

    commit di terminal 2

---
+++ SwiftulBasicBootcamp.playground/Pages/Functions-Example.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func itemStatus (itemName: String?, itemDesc: String?, itemQty: Int?, itemOrigin: Bool?) {
+    
+//    if let itemName, let itemDesc, let itemQty, let itemOrigin {
+//        return "true"
+//    } else {
+//        return "false"
+//    }
+}
+
+//print(itemStatus(itemName: "A", itemDesc: "B", itemQty: 3, itemOrigin: nil))
+
+func pythagoras(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+
+print(pythagoras(a: 3, b: 5))
