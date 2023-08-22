718f722131ae05f6b7963b50de4b8cec1922d9c6
Agung Nawawi <agung@fairtech.com.sg>
2023-08-22 14:26:42 +0700

    Belabor array dan loop

--- SwiftulBasicBootcamp.playground/Pages/training.xcplaygroundpage/Contents.swift
+++ SwiftulBasicBootcamp.playground/Pages/training.xcplaygroundpage/Contents.swift
@@ -1,3 +1,31 @@
 import Foundation
 
 // file training suka-suka
+
+
+// ######### ---------- Loop  ---------- ##########
+
+// Loop pada array
+let cityArray: [String] = ["Jakarta", "Bandung", "Makassar"]
+
+for item in cityArray {
+    print(item)
+}
+
+print("")
+
+// for Loop with where Clause
+// loop pada array dengan menggunakan where clause
+for cities in cityArray where cities != "Bandung" {
+    print(cities)
+}
+
+var jumlahArray = cityArray.count
+//print(jumlahArray)
+print("")
+
+// melakukan pengulangan dalam array dengan lebih dari satu parameter
+for (index, items) in cityArray.enumerated() {
+    print("Nilai dari Index = \(index) adalah \(items)")
+}
+
