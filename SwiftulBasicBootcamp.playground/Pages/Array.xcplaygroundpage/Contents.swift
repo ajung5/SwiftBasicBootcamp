f037b45a6d0740f212ce5e17e4b2feeb14b53c2a
Agung Nawawi <agung@fairtech.com.sg>
2023-08-22 10:46:04 +0700

    Penulisan Array

---
+++ SwiftulBasicBootcamp.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+// Array, Sets
+// let namaArray: [tipeData] = [isiArray]
+
+
+// cara penulisan Array
+// # 1
+let fruits: [String] = ["Banana", "Apple", "Orange"]
+
+// # 2
+let toyota: String = "Toyota"
+let honda: String = "Honda"
+let mercedes: String = "Mercedes-Benz"
+
+let cars: [String] = [toyota, honda, mercedes]
+
+// # 3
+// generic type <>
+let cars2: Array<String> = [toyota, honda, mercedes]
+print(cars2)
+
+
+
