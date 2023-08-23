54f2e3a48977daee3feb67f0367bdf72881077de
Agung Nawawi <agung@fairtech.com.sg>
2023-08-23 19:47:30 +0700

    Dictionary do not have index

--- SwiftulBasicBootcamp.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
+++ SwiftulBasicBootcamp.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
@@ -9,3 +9,20 @@ var userIsPremium: [String : Bool] = [
 
 // mengakses dictionary, kita panggil "key" nya
 let item = userIsPremium["User 1"]
+
+// dictionary adalah type safe
+// karena var/let pada saat memanggil dictionary adalah "optional"
+let item2 = userIsPremium["User 4"]  // output: nil, value "User 4" tidak ada dalam data.
+
+
+// ######### ---------- Dictionary do not have index---------- ##########
+
+var zipCode: [Int : String] = [
+    41261 : "Purwadadi",
+    0     : "Tidak Ada",
+    41252 : "Pagaden Barat"
+]
+
+let item3 = zipCode[2] // output: nil, karena dalam pemanggilan value dictionary bukan berdasarkan index
+
+
