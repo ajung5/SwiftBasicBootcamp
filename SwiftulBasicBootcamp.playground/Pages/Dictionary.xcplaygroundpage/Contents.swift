f05d6b2d9dcef8f13b805afa05b21fdd2bcc03cd
Agung Nawawi <agung@fairtech.com.sg>
2023-08-23 19:56:08 +0700

    Dictionary keys must be unique

--- SwiftulBasicBootcamp.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
+++ SwiftulBasicBootcamp.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
@@ -23,6 +23,18 @@ var zipCode: [Int : String] = [
     41252 : "Pagaden Barat"
 ]
 
-let item3 = zipCode[2] // output: nil, karena dalam pemanggilan value dictionary bukan berdasarkan index
+let item3 = zipCode[2] // output: nil, karena dalam pemanggilan value dictionary bukan berdasarkan index, tapi berdasarkan key
 
 
+// ######### ---------- Dictionary keys must be unique ---------- ##########
+
+// dalam Dictionary, key harus bersifat unik. jadi tidak boleh ada duplikat key, kalo duplikat value boleh
+
+var testScores: [String : Int] = [
+    "KD01" : 80,
+    "KD02" : 75,
+   // "KD01" : 85, // Dictionary literal of type '[String : Int]' has duplicate entries for string literal key 'KD01'
+    "KD03" : 80 // memiliki duplikat value, boleh
+]
+
+let score = testScores["KD01"]
