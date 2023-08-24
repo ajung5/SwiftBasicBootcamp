bad97f91458e852c644e14c88a2a8c44d4c81222
Agung Nawawi <agung@fairtech.com.sg>
2023-08-24 12:58:28 +0700

    Loop in array

---
+++ SwiftulBasicBootcamp.playground/Pages/ForLoops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+for item in 1...5 {
+    print(item)
+}
+
+// ######### ---------- Loop in Array ---------- ##########
+let capitalCity: [String] = ["Jakarta", "Tokyo", "Kuala Lumpur", "Jeddah", "Cape Town", "Berlin"]
+
+// memanggil array dengan loop
+for city in capitalCity {
+    print(city)
+}
