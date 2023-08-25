9f8da26191ca8cba7e746bf04d42c6fa3466d111
Agung Nawawi <agung@fairtech.com.sg>
2023-08-25 16:42:16 +0700

    Creating data Filter 2

--- SwiftulBasicBootcamp.playground/Pages/FilterSortMap.xcplaygroundpage/Contents.swift
+++ SwiftulBasicBootcamp.playground/Pages/FilterSortMap.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ struct UserDatabase {
 // buat array untuk menampung mengisi data ke "UserDatabase"
 let allUsers: [UserDatabase] = [
     UserDatabase(id: 3, name: "John Doe", isPremium: true),
-    UserDatabase(id: 1, name: "Smith", isPremium: false),
+    UserDatabase(id: 1, name: "Smith", isPremium: true),
     UserDatabase(id: 4, name: "Asep Stoberi", isPremium: true),
     UserDatabase(id: 2, name: "Dadang Konelo", isPremium: false),
     UserDatabase(id: 13, name: "Jane Doe", isPremium: true),
@@ -34,13 +34,33 @@ let allUsers: [UserDatabase] = [
 //}
 //print(allPremiumUsers)
 
-// # cara otomatis
+// # cara otomatis 1
 // sebenernya swift IDE udah nyediain method untuk filtering array
+//var allUserIsPremium: [UserDatabase] = allUsers.filter { user in
+//    if user.isPremium {
+//        return true
+//    }
+//
+//    return false
+//}
+//
+
+// # cara otomatis 2
 var allUserIsPremium: [UserDatabase] = allUsers.filter { user in
-    if user.isPremium {
-        return true
-    }
-    
-    return false
+    return user.isPremium
 }
-print(allUserIsPremium)
+for (_, item) in allUserIsPremium.enumerated() {
+    print("ID: \(item.id), Nama: \(item.name), Has Premium Account: \(item.isPremium)")
+}
+
+
+print("")
+
+// # cara otomatis 3
+// user ddigantikan dengan $0
+var allUserIsPremium2: [UserDatabase] = allUsers.filter({$0.isPremium})
+
+// hasilnya masih belum berurutan, karena cuma di filter. belum di sort
+for (_, item) in allUserIsPremium2.enumerated() {
+    print("ID: \(item.id), Nama: \(item.name), Has Premium Account: \(item.isPremium)")
+}
